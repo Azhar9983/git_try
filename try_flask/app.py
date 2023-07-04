045bf70b0b911a5ddcf4090c504207a2d6f57dbb
Azharuddin Ansari <azharuddinansari232@gmail.com>
2023-07-04 12:23:48 +0530

    Added variable uses in urls of webpages for int, float and string types.

--- try_flask/app.py
+++ try_flask/app.py
@@ -1,7 +1,24 @@
 from flask import Flask, redirect, render_template, url_for
 app = Flask(__name__)
 
+#for rendering webpages use templates
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#for variables insertions
+
+@app.route('/hello/<user_name>')
+def greet_user(user_name):
+    return 'Hello  %s' %user_name
+
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+    return 'Blog Number %d' %postID
+
+@app.route('/rev/<float:revNo>')
+def revision(revNo):
+    return 'Revision Number %f' %revNo
+
 
